221e7677ed47ce8c87880706249f12faeddaa1ba
finagjon <dafinagjonbalaj@gmail.com>
2021-08-15 19:43:01 -0400

    manage data with functions, variables, and operators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,33 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+// create function 
+var fight = function() {
+    // alert players they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+
+    // log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    // log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
+// execute function
+fight();
